6811d7be41b7d43adb80606fbf145fe49496b6e3
Eshap09 <67894889+Eshap09@users.noreply.github.com>
2026-04-23 15:42:48 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,26 @@
+class KthLargest {
+public:
+    priority_queue<int,vector<int>, greater<int>>pq;
+    int k;
+    KthLargest(int k, vector<int>& nums) {
+        this->k= k;
+       for(int i=0;i<nums.size();i++){
+          if(pq.size()<k){
+            pq.push(nums[i]);
+          } else if(pq.top()<=nums[i]){
+            pq.pop();
+            pq.push(nums[i]);
+          }
+       } 
+    }
+    
+    int add(int val) {
+         if(pq.size()<k){
+            pq.push(val);
+          } else if(pq.top()<=val){
+            pq.pop();
+            pq.push(val);
+          }
+          return pq.top();
+    }
+};
